1528c571f110cfa697d611ecca8cb715647e6e1f
Jim Doran <jimdoran26@gmail.com>
2021-09-07 10:22:12 -0400

    got events in a list form

--- hello_world.py
+++ hello_world.py
@@ -4,8 +4,26 @@ from bs4 import BeautifulSoup
 URL = "https://stevensducks.com/sports/mens-soccer/stats/2021/no-23-ithaca-college/boxscore/13121"
 r = requests.get(URL)
 
-soup = BeautifulSoup(r.content, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib
+soup = BeautifulSoup(r.content, 'html5lib')
 #print(soup.prettify())
 
-a= soup.find("td", text="00:00 :").find_next_sibling("td").text
-print(a)
+
+# kill all script and style elements
+for script in soup(["script", "style"]):
+    script.extract()    # rip it out
+
+# get text
+text = soup.get_text()
+
+# break into lines and remove leading and trailing space on each
+lines = (line.strip() for line in text.splitlines())
+# break multi-headlines into a line each
+chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+# drop blank lines
+text = '\n'.join(chunk for chunk in chunks if chunk)
+
+#print(text)
+
+list_of_events = text.split('\n')
+for row in list_of_events:
+    print(row)
